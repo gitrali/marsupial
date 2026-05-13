3b79cdf935f3e331f9308dce4a94124e426ff6a2
gitrali <visrali+git@disroot.org>
2026-05-13 22:26:38 +0800

    MORE RUST!!! MORE!!

---
+++ rust/playground/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb.d
@@ -0,0 +1,5 @@
+/home/dickcheese/project-possums/rust/playground/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb.d: src/main.rs
+
+/home/dickcheese/project-possums/rust/playground/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb: src/main.rs
+
+src/main.rs:
---
+++ rust/playground/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/dickcheese/project-possums/rust/playground/guessing_game/target/debug/guessing_game: /home/dickcheese/project-possums/rust/playground/guessing_game/src/main.rs
