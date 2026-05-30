30be96fe54513962193e586bc47772d8e5601597
gitrali <visrali+git@disroot.org>
2026-05-30 21:54:47 +0800

    major restructuring of the folders, also some new art and some other shit

--- marsupial/scripts/mouse_capture.gd
+++ marsupial/scripts/mouse_capture.gd
@@ -1,3 +1,13 @@
-class_name mousecapturecomponent extends Node3D
+class_name mouse_capture extends Node3D # this module attaches to it's parent.
 
+# debug toggle
 @export var debug: bool = false
+
+# the category for mouse capture settings
+@export_category("mouse_capture_settings")
+@export var current_mouse_mode : Input.MouseMode = Input.MOUSE_MODE_CAPTURED # sets the mouse input mode
+@export var mouse_sensitivity : float = 0.005 # mouse sensitivity, unsure what kindof unit the se are, but it works
+
+# variables used for mouse capture
+var _capture_mouse : bool
+var _mouse_input : Vector2
--- mouse_capture.gd
+++
@@ -1,3 +0,0 @@
-class_name mousecapturecomponent extends Node3D
-
-@export var debug: bool = false
